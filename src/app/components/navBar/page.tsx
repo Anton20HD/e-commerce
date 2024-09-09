5ac7b68f3313e7810962253350aabbae80e37f0b
Anton20HD <anton.haglund@iths.se>
2024-09-09 10:59:05 +0200

    Add buttons and icon to header, positioning

--- src/app/components/navBar/page.tsx
+++ src/app/components/navBar/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "@/app/components/navBar/page.module.scss";
+import homeIcon from "@/app/assets/beast.svg";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className={styles.headerContent}>
+      <div className={styles.iconContent}>
+        <img className={styles.icon} src={homeIcon.src} alt="" />
+      </div>
+      <div className={styles.buttonContent}>
+        <button className={styles.headerButtons}>Men's</button>
 
-export default NavBar
+        <button className={styles.headerButtons}>Women's</button>
+
+        <button className={styles.headerButtons}>Accessories</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
