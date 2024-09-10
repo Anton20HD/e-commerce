1a7ca792f75ed61cb4edea307698087bb7e85a14
Anton20HD <anton.haglund@iths.se>
2024-09-10 11:55:27 +0200

    Done with styling for searchbar

--- src/app/components/searchBar/page.tsx
+++ src/app/components/searchBar/page.tsx
@@ -1,43 +1,41 @@
+"use client";
 
-"use client"
-
-import React, {useState} from 'react';
-import TextField from "@mui/material/TextField";
-import styles from "./page.module.scss"
-import {useRouter} from 'next/navigation';
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
-
-
+import React, { useState } from "react";
+import styles from "./page.module.scss";
+import { useRouter } from "next/navigation";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-  const [searchQuery,setSearchQuery] = useState<string>('');
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const router = useRouter();
-  
 
- 
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 260,
+        backgroundColor: "#f4f4f4",
+        boxShadow: "none",
+        borderRadius: "5px"
+      }}
+    >
+      <IconButton sx={{ p: "10px" }} aria-label="menu">
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="What are you looking for?.."
+        inputProps={{ "aria-label": "search google maps" }}
+      />
+     
+    </Paper>
+  );
+};
 
-    return (
-        <form>
-          <TextField
-            sx={{ display: 'flex', alignItems: 'flex-end' }}
-            className={styles.searchBar}
-            id="outlined-basic"
-            variant="filled"
-            label="What are you looking for?.." 
-            type="search"
-            slotProps={{
-                input: {
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchIcon />
-                    </InputAdornment>
-                  ),
-                },
-              }}  
-          />
-        </form>
-    );
-    };
-    
-    export default SearchBar;
+export default SearchBar;
