aa24bef8ef7987686387f2303c0cf7962b983fd3
Anton20HD <anton.haglund@iths.se>
2024-09-12 10:45:16 +0200

    Create a underline on what category you choose

--- src/app/components/navBar/page.tsx
+++ src/app/components/navBar/page.tsx
@@ -1,38 +1,51 @@
-import React from "react";
+
+"use client"
+
+import React, { useState } from "react";
 import styles from "@/app/components/navBar/page.module.scss";
 import homeIcon from "@/app/assets/beast.svg";
 import SearchBar from "../searchBar/page";
-import HeartIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import PersonIcon from '@mui/icons-material/PersonOutlineOutlined';
-import CartIcon from '@mui/icons-material/LocalMallOutlined';
+import HeartIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import PersonIcon from "@mui/icons-material/PersonOutlineOutlined";
+import CartIcon from "@mui/icons-material/LocalMallOutlined";
 
 const NavBar = () => {
+
+const [activeLink,setActiveLink] = useState<string | null>("")
+
+const handleLinkClick = (link:string) => {
+    setActiveLink(link);
+  };
+
+
+
   return (
     <div className={styles.headerContent}>
       <div className={styles.iconContent}>
         <img className={styles.icon} src={homeIcon.src} alt="" />
       </div>
+
       <div className={styles.buttonContent}>
-        <button className={styles.headerButtons}>Men's</button>
+        <button className={`${styles.headerButtons} ${activeLink === "mens" ? styles.active : ""}`}  onClick={() => handleLinkClick("mens")}>MEN'S</button>
 
-        <button className={styles.headerButtons}>Women's</button>
+        <button className={`${styles.headerButtons} ${activeLink === "womens" ? styles.active : ""}`} onClick={() => handleLinkClick("womens")}>WOMEN'S</button>
 
-        <button className={styles.headerButtons}>Accessories</button>
+        <button className={`${styles.headerButtons} ${activeLink === "accessories" ? styles.active : ""}`} onClick={() => handleLinkClick("accessories")}>ACCESSORIES</button>
       </div>
       <div className={styles.searchBarContent}>
         <SearchBar />
       </div>
       <div className={styles.iconsContent}>
         <div className={styles.iconButton}>
-        <HeartIcon/>
+          <HeartIcon />
         </div>
         <div className={styles.iconButton}>
-        <PersonIcon/>
+          <PersonIcon />
         </div>
         <div className={styles.iconButton}>
-        <CartIcon/>
+          <CartIcon />
         </div>
-        </div>
+      </div>
     </div>
   );
 };
