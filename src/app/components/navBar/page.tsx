84deeef74edecaa7a61a96ed6e53a920a4393743
Anton20HD <anton.haglund@iths.se>
2024-09-16 11:37:41 +0200

    Create dropdown menu for men

--- src/app/components/navBar/page.tsx
+++ src/app/components/navBar/page.tsx
@@ -1,5 +1,4 @@
-
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import styles from "@/app/components/navBar/page.module.scss";
@@ -10,14 +9,11 @@ import PersonIcon from "@mui/icons-material/PersonOutlineOutlined";
 import CartIcon from "@mui/icons-material/LocalMallOutlined";
 
 const NavBar = () => {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
 
-const [activeLink,setActiveLink] = useState<string | null>("")
-
-const handleLinkClick = (link:string) => {
+  const handleLinkClick = (link: string) => {
     setActiveLink(link);
   };
-
-
 
   return (
     <div className={styles.headerContent}>
@@ -26,7 +22,29 @@ const handleLinkClick = (link:string) => {
       </div>
 
       <div className={styles.buttonContent}>
-        <button className={styles.headerButtons}>MEN'S</button>
+        <div className={styles.dropdown}>
+          <button
+            className={`${styles.headerButtons} ${
+              activeLink === "mens" ? styles.active : ""
+            }`}
+            onMouseEnter={() => handleLinkClick("mens")}
+          >
+            MEN'S
+          </button>
+          <div className={styles.dropdownMenu}>
+              <div>
+                <h3 className="dropdownHeading">Clothes</h3>
+                <div className={styles.dropdownLinks}>
+                  <a className={styles.link} href="#">All</a>
+                  <a className={styles.link} href="#">T-shirts & Tops</a>
+                  <a className={styles.link} href="#">Hoodies</a>
+                  <a className={styles.link} href="#">Shorts</a>
+                  <a className={styles.link} href="#">Sweaters</a>
+
+                </div>
+              </div>
+          </div>
+        </div>
 
         <button className={styles.headerButtons}>WOMEN'S</button>
 
