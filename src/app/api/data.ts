71562089246e0446a91175f45e23641273e21ec1
Anton20HD <anton.haglund@iths.se>
2024-09-19 11:39:47 +0200

    Create a data file to store products and add t shirt images

---
+++ src/app/api/data.ts
@@ -0,0 +1,27 @@
+
+
+
+export const products = [
+    {
+        _id: 1,
+        title: "White T shirt",
+        price: 65,
+        image: "",
+        category: "Men",
+        sizes:["S","M","L"],
+        popular: false
+    },
+    {
+        _id: 2,
+        title: "Black T shirt",
+        price: 85,
+        image: "",
+        category: "Men",
+        sizes:["S","M","L"],
+        popular: false
+
+    }
+   
+
+];
+  
