1bd518a5a31a05baced4aec1ac7b8d0eaa787aac
Anton20HD <anton.haglund@iths.se>
2024-09-30 17:22:04 +0200

    Add styling for footer and positioning

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./page.module.scss";
-import homeIcon from "@/app/assets/beast.svg"
+import homeIcon from "@/app/assets/beast.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,54 +14,78 @@ import NewsLetterBar from "../newsLetterbar/page";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+       <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
       <section className={styles.footerContainer}>
-        <div className={styles.homeIcon}>
-        <img className={styles.icon} src={homeIcon.src} alt="" />
+        <div className={styles.contentWrapper}>
+          <div className={styles.homeIcon}>
+            <img className={styles.icon} src={homeIcon.src} alt="" />
+          </div>
 
+          <div className={styles.shopSection}>
+            <h2 className={styles.title}>Shop</h2>
+            <p>Mens</p>
+            <p>Womens</p>
+            <p>Accessories</p>
+          </div>
+          <div className={styles.accountSection}>
+            <h2 className={styles.title}>My account</h2>
+            <p>Login</p>
+            <p>Register</p>
+          </div>
         </div>
+       
 
-        <div className={styles.shopSection}>
-           <h2>Shop</h2>
-           <p>Mens</p>
-           <p>Womens</p>
-           <p>Accessories</p>
-        </div>
-        <div className={styles.accountSection}>
-          <h2>My account</h2>
-          <p>Login</p>
-          <p>Register</p>
-        </div>
         <div className={styles.socialIconsSection}>
           <div className={styles.newsLetterSection}>
-            <h2>Newsletter</h2>
+            <h2 className={styles.title}>Newsletter</h2>
             <p>Subscribe to our newsletter!</p>
-            <NewsLetterBar/>
-
+            <NewsLetterBar />
           </div>
-          <ul className={styles.ul}>
-            <li>
-              <a href="#" className={styles.instagramColor}>
-                <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.youtubeColor}>
-                <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.facebookColor}>
-                <FontAwesomeIcon icon={faFacebookF} className={styles.icon} />
-              </a>
-            </li>
-            <li>
-              <a href="#" className={styles.tiktokColor}>
-                <FontAwesomeIcon icon={faTiktok} className={styles.icon} />
-              </a>
-            </li>
-          </ul>
+          <div className={styles.ul}>
+        <a
+          className={styles.socialButton}
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faInstagram}  size="lg" />
+        </a>
+
+        <a
+          className={styles.socialButton}
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+
+        <a
+          className={styles.socialButton}
+          href="https://www.tiktok.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTiktok} />
+        </a>
+
+        <a
+          className={styles.socialButton}
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebookF} className={styles.icon} size="xs" />
+        </a>
+      </div>
         </div>
       </section>
+      <div className={styles.copyrightSection}>
+        <p> © 2024 GymBeast. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
