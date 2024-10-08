f39c2eaa8c7f98ba8991018df5e18401b279c119
Anton20HD <anton.haglund@iths.se>
2024-10-08 18:26:08 +0200

    Success mongoose connection

--- src/libs/db/mongodb.js
+++ src/libs/db/mongodb.js
@@ -1,21 +1,17 @@
 import mongoose from "mongoose";
 
-
-
 const connectDB = async () => {
   try {
-
-    await mongoose.connect("mongodb+srv://antonhaglund20:zFSZKv9LEEpqV2BZ@ecommerce.xlt8q.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
+    await mongoose.connect(
+      "mongodb+srv://antonhaglund20:zFSZKv9LEEpqV2BZ@ecommerce.xlt8q.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce"
+    );
     console.log("Successfully connected to DB!");
   } catch (error) {
     console.log(error);
     process.exit(1);
-
   }
 };
 
 export default connectDB;
+
+connectDB();
