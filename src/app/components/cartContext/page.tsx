c2c367dd31c2a5fb6a0a1dba69bcb821daf8d578
Anton20HD <anton.haglund@iths.se>
2024-10-31 12:51:07 +0100

    Create cartcontext file to handle cart logic and add new item

---
+++ src/app/components/cartContext/page.tsx
@@ -0,0 +1,56 @@
+// Good way to manage global state(especially where you need access to multiple components)
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface CartItem {
+  _id: string;
+  name: string;
+  price: number;
+  size: string;
+  quantity: number;
+}
+
+// functions go here
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find(
+        (cartItem) => cartItem._id === item._id && cartItem.size === item.size
+      );
+
+      if (existingItem) {
+        //update quantity if item is already in cart with same size
+        return prevCart.map((cartItem) =>
+          cartItem._id === item._id && cartItem.size === item.size
+            ? { ...cartItem, quantity: cartItem.quantity + item.quantity }
+            : cartItem
+        );
+      } else {
+        //add new item to cart
+        return [...prevCart, item];
+      }
+    });
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error();
+  }
+  return context;
+};
--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -3,8 +3,11 @@ import styles from "@/app/components/header/page.module.scss";
 import homeIcon from "@/app/assets/GymBeast.svg";
 import SearchBar from "../searchBar/page";
 import HeartIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import HeartIconFilled from '@mui/icons-material/Favorite';
 import PersonIcon from "@mui/icons-material/PersonOutlineOutlined";
+import PersonIconFilled from '@mui/icons-material/Person';
 import CartIcon from "@mui/icons-material/LocalMallOutlined";
+import CartIconFilled from '@mui/icons-material/LocalMall';
 import ButtonContent from "../buttons/page";
 import Link from "next/link";
 
