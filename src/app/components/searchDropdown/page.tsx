ee32bb1248d274192b3cf790ab1307ce885379f7
Anton20HD <anton.haglund@iths.se>
2024-11-17 15:07:08 +0100

    Add search functionality in dropdown and filter through products

--- src/app/components/searchDropdown/page.tsx
+++ src/app/components/searchDropdown/page.tsx
@@ -10,83 +10,101 @@ import { useSearch } from "../searchContext/page";
 import styles from "@/app/components/searchDropdown/page.module.scss";
 import HeartIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import SearchBar from "../searchBar/page";
-import homeIcon from "@/app/assets/GymBeast.svg"
+import homeIcon from "@/app/assets/GymBeast.svg";
 import CloseIcon from "@mui/icons-material/Close";
-
 
 interface SearchDropdownProps {
   toggleDropdown: () => void;
   isVisible: boolean;
 }
 
-const ProductSearchDropdown = ({toggleDropdown, isVisible}: SearchDropdownProps) => {
+const ProductSearchDropdown = ({
+  toggleDropdown,
+  isVisible,
+}: SearchDropdownProps) => {
   const { searchTerm, setSearchTerm, filteredProducts } = useSearch();
   const router = useRouter();
 
   const handleProduct = (productId: string) => {
+    toggleDropdown();
+    setSearchTerm("");
     router.push(`/products/${productId}`);
   };
 
+
+  const handlecloseDropdown = () => {
+
+      toggleDropdown();
+      setSearchTerm("");
+  }
+
   return (
-    <div className={`${styles.searchDropdownContainer} ${isVisible ? styles.open : ""}`}>
+    <div
+      className={`${styles.searchDropdownContainer} ${
+        isVisible ? styles.open : ""
+      }`}
+    >
       <div className={styles.searchBarContent}>
-    <div className={styles.iconContent}>
-            <img className={styles.icon} src={homeIcon.src} alt="Home" />
-      </div>
-      <Paper
-        component="form"
-        className={styles.searchBar}
-        sx={{
-          p: "2px 4px",
-          display: "flex",
-          alignItems: "center",
-          width: 400,
-          backgroundColor: "#f4f4f4",
-          boxShadow: "none",
-          borderRadius: "5px",
-        }}
-      >
-        <IconButton sx={{ p: "10px" }} aria-label="menu">
-          <SearchIcon />
-        </IconButton>
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="What are you looking for?.."
-          inputProps={{ "aria-label": "search clothes" }}
-        />
-      </Paper>
-    
-      <div className={styles.navCloseIconContent} >
-      <div className={styles.iconWrapper} onClick={toggleDropdown}>
-
-          <CloseIcon className={styles.closeIcon}></CloseIcon>
+        <div className={styles.iconContent}>
+          <img className={styles.icon} src={homeIcon.src} alt="Home" />
         </div>
-      </div>
-      </div>
+        <Paper
+          component="form"
+          className={styles.searchBar}
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+            backgroundColor: "#f4f4f4",
+            boxShadow: "none",
+            borderRadius: "5px",
+          }}
+        >
+          <IconButton sx={{ p: "10px" }} aria-label="menu">
+            <SearchIcon />
+          </IconButton>
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="What are you looking for?.."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            inputProps={{ "aria-label": "search clothes" }}
+          />
+        </Paper>
 
-      {/* <div className={styles.productSection}>
-      {filteredProducts.map((product) => (
-        <div key={product._id} onClick={() => handleProduct(product._id)}>
-          <div className={styles.productCard}>
-            <div className={styles.buttonContent}>
-              <button className={styles.wishList}>
-                <HeartIcon className={styles.heartIcon} />
-              </button>
-            </div>
-            {product.image.map((imgUrl, index) => (
-              <img
-                key={index}
-                src={imgUrl}
-                alt={product.name}
-                className={styles.productImage}
-              />
-            ))}
+        <div className={styles.navCloseIconContent}>
+          <div className={styles.iconWrapper} onClick={handlecloseDropdown}>
+            <CloseIcon className={styles.closeIcon}></CloseIcon>
           </div>
-          <h2 className={styles.productName}>{product.name}</h2>
-          <p className={styles.productPrice}>{product.price} kr</p>
         </div>
-      ))}
-    </div> */}
+      </div>
+
+      <div className={styles.productSection}>
+        {searchTerm && filteredProducts.length === 0 && <p> </p>}
+        {searchTerm &&
+          filteredProducts.map((product) => (
+            <div key={product._id} onClick={() => handleProduct(product._id)}>
+              <div className={styles.productCard}>
+                <div className={styles.buttonContent}>
+                  <button className={styles.wishList}>
+                    <HeartIcon className={styles.heartIcon} />
+                  </button>
+                </div>
+                {product.image.map((imgUrl, index) => (
+                  <img
+                    key={index}
+                    src={imgUrl}
+                    alt={product.name}
+                    className={styles.productImage}
+                  />
+                ))}
+              </div>
+              <h2 className={styles.productName}>{product.name}</h2>
+              <p className={styles.productPrice}>{product.price} kr</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
