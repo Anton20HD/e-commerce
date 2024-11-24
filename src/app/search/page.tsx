fa2bef99ca248480047ecb6d5fbf4c268e989d70
Anton20HD <anton.haglund@iths.se>
2024-11-24 19:46:57 +0100

    Add img to searchPage. Apply styling to different components

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,18 +2,36 @@
 
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import styles from "@/app/search/page.module.scss";
+import noResultsImage from "@/app/assets/search-no-results.png";
+import Link from "next/link";
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
   const query = searchParams.get("q") || "";
 
   return (
-    <div>
-      <h1>No Results Found</h1>
-      <p>
-        Sorry, we couldn't find any results for "<strong>{query}</strong>"
-      </p>
-      <p>Please try searching with a different term.</p>
+    <div className={styles.searchPageContainer}>
+      <img src={noResultsImage.src} alt="" />
+      <div className={styles.contentWrapper}>
+        <h1 className={styles.searchPageTitle}>No Results Found</h1>
+        <div className={styles.searchPageContent}>
+          <p className={styles.noResultsText}>
+            Sorry, we couldn't find any results for "
+            <span className={styles.searchQueryWord}>{query}</span>".
+          </p>
+          <p className={styles.noResultsText}>
+            Please try searching with a different term.
+          </p>
+          <div className={styles.buttonContent}>
+            <Link href="/">
+              <button className={styles.searchPageButton}>
+                Go to HomePage
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
