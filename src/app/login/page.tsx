9c804af389c56d983ab722b1dcf778f076bb6bee
Anton20HD <anton.haglund@iths.se>
2024-12-12 11:24:02 +0100

    Add homeicon and title in loginPage and fix position

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from '@/app/login/page.module.scss'
+import homeIcon from '@/app/assets/GymBeast.svg'
 
-const page = () => {
+
+const LoginPage = () => {
   return (
-    <div>page</div>
+    <div className={styles.loginContainer}>
+        <div className={styles.loginContent}>
+            
+            <img className={styles.icon} src={homeIcon.src} alt="" />
+            <h1>Gymbeast Login</h1>
+
+        </div>
+    </div>
   )
 }
 
-export default page
+export default LoginPage
