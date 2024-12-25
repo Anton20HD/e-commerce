4c58e78a29e5f4a43d9c1f9e3a6e5b40c760a6dd
Anton20HD <anton.haglund@iths.se>
2024-12-25 16:55:41 +0100

    Add basic validation when logging in. Fetch data from login in api and store token in localstorage when verifying

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+
+"use client"
+
+import React, { useState} from "react";
 import styles from "@/app/login/page.module.scss";
 import homeIcon from "@/app/assets/GymBeast.svg";
 import Link from "next/link";
+import { useRouter} from "next/navigation";
 
 const LoginPage = () => {
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+  try {
+    const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    },
+    body: JSON.stringify({email, password}),
+    })
+
+    const data = await response.json();
+
+    if(response.ok) {
+      //store token in localstorage
+      localStorage.setItem("token", data.token);
+      setError(null);
+    console.log("Login successful!")
+
+    //Redirect user to homepage after login
+    router.push("/")
+    } else {
+
+      setError(data.message || "Invalid credentials. Please try again");
+    }
+  } catch(err) {
+    console.error("Error during login:", err);
+    setError("An error occured. Please try again later")
+  }
+  
+}
+  
+  
+  
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginContent}>
@@ -11,7 +55,7 @@ const LoginPage = () => {
           <img className={styles.icon} src={homeIcon.src} alt="Home" />
         </div>
         <h1 className={styles.loginTitle}>Gymbeast Login</h1>
-        <form method="post" action={""} className={styles.orderInfo}>
+        <form  onSubmit={handleSubmit}className={styles.orderInfo}>
           <div className={styles.inputInfo}>
             <div className={styles.shippingSection}>
               <h2 className={styles.shippingTitle}></h2>
@@ -22,19 +66,30 @@ const LoginPage = () => {
               type="text"
               placeholder="Email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               className={styles.loginLabel}
               type="text"
               placeholder="Password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <button type="submit" className={styles.loginButton}>
               Login
             </button>
+            {error && <p className={styles.errorText}>{error}</p>}
             <div className={styles.registrationSection}>
-            <h3 className={styles.registrationText}>Don't have an account? <Link href={"/register"}><span className={styles.registrationSpanText}>Sign up here!</span></Link></h3>
-
+              <h3 className={styles.registrationText}>
+                Don't have an account?{" "}
+                <Link href={"/register"}>
+                  <span className={styles.registrationSpanText}>
+                    Sign up here!
+                  </span>
+                </Link>
+              </h3>
             </div>
           </div>
         </form>
