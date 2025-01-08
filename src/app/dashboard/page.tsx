380c3ab56a5c953770152ee2a5d38c5eda957ef6
Anton20HD <anton.haglund@iths.se>
2025-01-08 09:42:40 +0100

    Implement functionality to dynamically change maincontent in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,26 @@
 import React, { useEffect, useState } from "react";
 import styles from "@/app/dashboard/page.module.scss";
 import { useRouter } from "next/navigation";
+import HeartIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import ReceiptIcon from "@mui/icons-material/Receipt";
 
 const DashboardPage = () => {
   const router = useRouter();
+  const [activeLink, setActiveLink] = useState<"orders" | "wishlist" | "signout">("orders");
+
+  // const renderContent = () => {
+  //   switch (activeLink) {
+  //     case "orders":
+  //       return <Orders />;
+  //     case "wishlist":
+  //       return <Wishlist />;
+  //     case "signout":
+  //       return <SignOut />;
+  //     default:
+  //       return <Orders />;
+  //   }
+  // };
 
   useEffect(() => {
     //Check user authentication
@@ -25,30 +42,61 @@ const DashboardPage = () => {
       <div className={styles.dashboardSection}>
         <div className={styles.profileOverviewSection}>
           <div className={styles.orderInfoSection}>
-            <h4>Orders</h4>
-
-
+            <div
+              className={`${styles["itemWrapper"]} ${
+                activeLink === "orders" ? styles["active"] : ""
+              }`}
+              onClick={() => setActiveLink("orders")}
+            >
+              <ReceiptIcon />
+              Orders
+            </div>
           </div>
           <div className={styles.wishlistSection}>
-            <h4>Wishlist</h4>
-
-
+            <div
+              className={`${styles["itemWrapper"]} ${
+                activeLink === "wishlist" ? styles["active"] : ""
+              }`}
+              onClick={() => setActiveLink("wishlist")}
+            >
+              <HeartIcon />
+              Wishlist
+            </div>
           </div>
           <div className={styles.signOutSection}>
-              <h4>Sign out</h4>
-
-
-
+            <div
+              className={`${styles["itemWrapper"]} ${
+                activeLink === "signout" ? styles["active"] : ""
+              }`}
+              onClick={() => setActiveLink("signout")}
+            >
+              <ExitToAppIcon />
+              Signout
+            </div>
           </div>
         </div>
         <div className={styles.accountContent}>
-       
+        {activeLink === 'orders' && 
+        <div>
+          Orders Content
           
-
+          </div>}
+        {activeLink === 'wishlist' && 
+        <div>
+          
+          Wishlist Content
+          
+          </div>}
+        {activeLink === 'signout' && 
+        <div>
+          
+          Sign Out Content
+          </div>}
         </div>
       </div>
     </div>
   );
 };
+
 
 export default DashboardPage;
