75838b0b9aeda3ec739b9fc3b12b8b05dcc3847f
Anton20HD <anton.haglund@iths.se>
2025-01-26 17:37:14 +0100

    Create individual accessory id page and basic structure in accessory page

---
+++ src/app/accessories/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "@/app/products/page.module.scss";
+import CartIcon from "@mui/icons-material/LocalMallOutlined";
+import HeartIcon from "@mui/icons-material/FavoriteBorderOutlined";
+//import { useSearch } from "../components/searchContext/page";
+import { useWishlist } from "../components/wishlistContext/page";
+import HeartOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import HeartFilledIcon from "@mui/icons-material/Favorite";
+
+
+interface Product {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string[];
+  sizes: string[];
+  soldout: boolean;
+  category: string;
+  subCategory: string;
+}
+
+
+interface Accessory extends Product {
+
+
+}
+
+const allAccessories = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  // const { searchTerm } = useSearch();
+  const router = useRouter();
+  const { addToWishlist, wishlist, removeFromWishlist } = useWishlist();
+  const [selectedSize, setSelectedSize] = useState("S");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/api/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const handleProduct = (accessoryId: string) => {
+    router.push(`/accessories/${accessoryId}`);
+  };
+
+  const handleWishlistToggle = (product: Product) => {
+    const isAlreadyInWishlist = wishlist.some((item) => item._id === product._id);
+
+    if (isAlreadyInWishlist) {
+      removeFromWishlist(product._id);
+    } else {
+      addToWishlist({
+        _id: product._id,
+        name: product.name,
+        price: product.price,
+        size: selectedSize,
+        image: product.image[0],
+      });
+    }
+  };
+
+
+  //const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  return (
+    <div className={styles.productSection}>
+      {products.map((product: Product) => {
+       const isWishlisted = wishlist.some((item) => item._id === product._id);
+
+       return (
+        <div key={product._id} onClick={() => handleProduct(product._id)}>
+          <div className={styles.productCard}>
+            <div className={styles.buttonContent}>
+              <button 
+              className={styles.wishList} 
+              onClick={(e) => {
+
+                e.stopPropagation(); // Prevent navigation to single product page when clicking
+                handleWishlistToggle(product);
+
+              }}
+              >
+                {isWishlisted ? (
+                <HeartFilledIcon sx={{ color: "black", fontSize: 20 }} />
+              ) : (
+                <HeartOutlinedIcon sx={{fontSize: 20}} />
+              )}
+              </button>
+            </div>
+            {product.image.map((imgUrl, index) => (
+              <img
+                key={index}
+                src={imgUrl}
+                alt={product.name}
+                className={styles.productImage}
+              />
+            ))}
+          </div>
+          <h2 className={styles.productName}>{product.name}</h2>
+          <p className={styles.productPrice}>{product.price} kr</p>
+        </div>
+       )
+     })}
+    </div>
+  );
+};
+
+
+export default allAccessories
